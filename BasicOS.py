cd07615cc44d975e42946d28318a7c8feb5c1a56
exoticDFT <exoticdft@gmail.com>
2019-02-18 22:08:48 -0500

    Adds some functionality to the BasicOS.py file. - Adds new functions to print warnings, errors and successes in colored   output. - Adds function to change the current working directory if the path   exists. - Adds functionality to load_file. - Cleans up formatting/output for load_file. - Adds function to exit the program with a message.

--- BasicOS.py
+++ BasicOS.py
@@ -1,14 +1,43 @@
 import os
+from string import Template
+from termcolor import colored
 
-def load_file(file_name, state="r"):
+def change_dir(dir_path, proceed=True):
+    try:
+        os.chdir(dir_path)
+    except Exception:
+        error_message = Template("The path ${path} does not exist.")
+        print_warn(error_message.substitute(path=dir_path))
+        if not proceed:
+            exit_program()
+
+
+def exit_program():
+    print_error("Cannot continue reliably. Exiting the program.")
+    quit()
+
+
+def load_file(file_name, state="r", proceed=True):
     try:
         f = open(file_name, state)
         return f
     except FileNotFoundError:
-        print("Error reading file. Did you specify the correct file and path?")
-        quit()
+        print_warn(
+            "Error reading file. Did you specify the correct file and path?"
+        )
+        if not proceed:
+            exit_program()
 
 
-def change_dir(path):
-    pass
+def print_error(msg):
+    print(colored(msg, "red"))
+
+
+def print_success(msg):
+    print(colored(msg, "green"))
+
+
+def print_warn(msg):
+    print(colored(msg, "yellow"))
+
 
